82937a78d3fe690b9ffca021c7318ad493460470
Haniff Amouei <h.amouei@web.de>
2022-06-01 18:56:02 +0200

    Added faker to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
 
-user01 = User.new(
-  email: "fccristino@hotmail.com",
-  password: "12345678"
-)
-user01.save
+# user01 = User.new(
+#   email: "fccristino@hotmail.com",
+#   password: "12345678"
+# )
+# user01.save
+Place.destroy_all
+User.destroy_all
 
-place01 = Place.new(
-  price: 100,
-  address: "Rua capitao menezes",
-  name: "Minha casa",
-  description: "Sejam bem vindos",
-  rating: 4.5,
-  user_id: 1
-)
-place01.save
+9.times do
+  user = User.new(
+    email: Faker::Internet.email,
+    password: Faker::Alphanumeric.alphanumeric(number: 10)
+  )
+  user.save
+    place = Place.new(
+      name: Faker::Company.name,
+      address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+      price: Faker::Number.between(from: 50, to: 200),
+      user: user
+    )
+    place.save
+end
+
+# user.save
+# place01 = Place.new(
+#   price: 100,
+#   address: "Rua capitao menezes",
+#   name: "Minha casa",
+#   description: "Sejam bem vindos",
+#   rating: 4.5,
+#   user_id: 1
+# )
+# place01.save
+
+9.times do
+  place = Place.new(
+    name: Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    price: Faker::Number.between(from: 50, to: 200),
+    user: @user
+  )
+  place.save
+end
