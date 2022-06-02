d0e2ace16e9fd9fedd835e560ab879793a2b2264
Guilherme Petkovic <gjrpetkovic@gmail.com>
2022-06-01 23:14:47 -0300

    refactored routes using resources for future nesting - also redirected home in navbar to places page

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,5 @@ Rails.application.routes.draw do
   root to: 'places#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get 'places', to: 'places#index'
-  get 'places/new', to: 'places#new', as: :new_places
-  post 'places', to: 'places#create'
-  get 'places/:id', to: 'places#show', as: :show
+  resources :places, only: [:index, :new, :create, :show ]
 end
