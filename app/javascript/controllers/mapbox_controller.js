1d368ca35bb9806b2612dc6044049a91cacd8504
Fabiano Catao <fccristino@hotmail.com>
2022-06-03 15:12:00 -0300

    EXCLAMATION

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -1,5 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
-import mapboxgl from "mapbox-gl"
+import mapboxgl from "!mapbox-gl"
 import "mapbox-gl/dist/mapbox-gl.css"
 
 export default class extends Controller {
